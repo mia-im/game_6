77dcdd557ae78490a5c56da2238b9fa4a84b3260
mia-im <62039441+mia-im@users.noreply.github.com>
2021-11-11 04:15:11 +0200

    Update Controlls.cs

--- Scripts/gaimScene/Controlls.cs
+++ Scripts/gaimScene/Controlls.cs
@@ -4,47 +4,27 @@ using UnityEngine;
 
 public class Controlls : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed;
     public float jumpForce;
     private float moveInput;
-    //private bool isGrounded = true;
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent <Rigidbody2D> ();
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        //движение вправво и влево
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-Vector2.right * speed * Time.deltaTime);
-        }
-        //прыжок
-        if (Input.GetKeyDown (KeyCode.Space))
-        {
-            rb.AddForce (Vector2.up * 3500);
-        }
-    }*/
+    //РІРїСЂР°РІРѕ РІР»РµРІРѕ
     private void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
     }
 
+    //РїСЂС‹Р¶РѕРє
     private void Update()
     {        
-        //прыжок
         if (Input.GetKeyDown (KeyCode.Space))
         {
-            //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
             rb.velocity = Vector2.up * jumpForce;
         }
     }
